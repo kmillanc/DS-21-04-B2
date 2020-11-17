ce9c6ef8a414e20bcc18bf266132ae92bbdbc114
k.millanc <k.millanc@udc.es>
2020-11-17 18:12:52 +0100

    Comienzo

---
+++ src/e1/TheLordOfTheRings.java
@@ -0,0 +1,16 @@
+package e1;
+
+public class TheLordOfTheRings {
+    public class dice{
+        public int value;
+//        public boolean getRandomNumber() {
+//            return Math.random();
+//        }
+    }
+    public class Character{
+        //public String name;
+        public int vida;
+        public int armadura;
+
+    }
+}
---
+++ test/e1/TheLordOfTheRingsTest.java
@@ -0,0 +1,17 @@
+package e1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TheLordOfTheRingsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
